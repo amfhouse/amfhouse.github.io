36daed98903c712108587c18f22b979d5e9146bb
Jemima <ah247@jszmail.com>
2020-02-20 15:10:26 +0200

    strat... UPLOAD

---
+++ scripts/tooltip.js
@@ -0,0 +1,13 @@
+
+
+  <script type="text/javascript">//<![CDATA[
+
+    $(window).load(function(){
+      
+$('.tooltip-demo.well').tooltip({
+  selector: "a[rel=tooltip]"
+})
+
+    });
+
+  //]]></script>
